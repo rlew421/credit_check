2ee089862c0b0ad0c807e5b9c6d65bf7aa333d40
Rachel Lew <rlew07@gmail.com>
2023-12-13 16:48:48 -0600

    Add number validation

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,7 +1,22 @@
+require 'pry'
+
 card_number = "5541808923795240"
 
-# Your Luhn Algorithm Here
+int_array = card_number.split("").map(&:to_i)
 
-# Output
-## If it is valid, print "The number [card number] is valid!"
-## If it is invalid, print "The number [card number] is invalid!"
+i = 0
+sum = 0
+int_array.each do |int|
+    int *= 2 if i.even?
+
+    int = (1 + (int - 10)) if int >= 10
+
+    sum += int
+    i += 1
+end
+
+if sum % 10 == 0
+    puts "The number #{card_number} is valid!"
+else
+    puts "The number #{card_number} is invalid!"
+end
